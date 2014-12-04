f006110417d2ef765a24dabfa7c028327d0633c9
moliri <lmaliakal@u.northwestern.edu>
2014-12-03 18:42:53 -0600

    iterating through csv

--- links.py
+++ links.py
@@ -3,21 +3,26 @@ import urllib
 import webbrowser
 
 f = open('shuffledLinks.csv')
-csv_f = csv.reader(f)
+csv_f_read = csv.reader(f)
 
 shuffleIDs = []
 groupIDs = []
 postIDs = []
 links = []
+visited = []
 
-for row in csv_f:
+for row in csv_f_read:
 	shuffleIDs.append(row[0])
 	groupIDs.append(row[1])
 	postIDs.append(row[2])
 	links.append(row[3])
 
-postLink = str(links[3])
-url = urllib.urlopen(postLink)
-page = url.read()
+for i in links:
+	if  visited[i] == 1: #if this row was visited already, move to the next row
+		i += 1
+	postURL = str(links[i]) #get the next link
+	#url = urllib.urlopen(postLink)
+	#page = url.read()
+	visited.append(1) #update the visited value
 
-webbrowser.open(postLink)
+webbrowser.open(postURL)
